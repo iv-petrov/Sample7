b2aa83cec9c626040d7f0cfaeaa666f2ee05bd32
Петров Игорь <petroviv@vtb.ru>
2025-04-18 13:01:41 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sample7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.WriteLine("Не задан файл для преобразования");
+                return;
+            }
+            string inputFileName = args[0];
+            if (!Path.Exists(inputFileName)) {
+                Console.WriteLine(@"Файл {inputFileName} не найден");
+                return;
+            }
+            byte[] inputFileBytes = File.ReadAllBytes(inputFileName);
+            string documentNumber;
+            if (args.Length > 1) 
+            {
+                documentNumber = args[1];
+            }
+            else
+            {
+                documentNumber = "Unknown";
+            }
+            // Ну поехали
+            PrintService.ProcessInputFile(inputFileBytes, inputFileName, documentNumber, DateTime.Now, false);
+            return;
+        }
+    }
+}
